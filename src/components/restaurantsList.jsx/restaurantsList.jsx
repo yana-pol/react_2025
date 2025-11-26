e8431f7372c7d8eb506c769e88cf01ce364f4ce8
yana-pol <matuninayy@cfuv.ru>
2025-11-26 15:53:50 +0300

    Add files via upload

--- src/components/restaurantsList.jsx/restaurantsList.jsx
+++ src/components/restaurantsList.jsx/restaurantsList.jsx
@@ -2,18 +2,17 @@ import { Restaurant } from "../Restaurant/restaurant";
 import { useState } from "react";
 
 export const RestaurantsList = ({ restaurants = [] }) => {
-  const [activeRest, setActiveRest] = useState(0);
-
+  const [activeRest, setActiveRest] = useState(restaurants[0].id);
   return (
     <>
       <ul style={{ display: "flex", gap: "5px" }}>
         {restaurants.map((rest) => (
           <li key={rest.id} style={{ listStyle: "none" }}>
-            <button onClick={() => setActiveRest(index)}>{rest.name}</button>
+            <button onClick={() => setActiveRest(rest.id)}>{rest.name}</button>
           </li>
         ))}
       </ul>
-      <Restaurant rest={restaurants[activeRest]} />
+      <Restaurant rest={restaurants.find((item) => activeRest === item.id)} />
     </>
   );
 };
