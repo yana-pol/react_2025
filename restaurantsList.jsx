feeeb78ca126dd5416906e31a215892da3229d62
yana-pol <matuninayy@cfuv.ru>
2025-11-26 15:54:52 +0300

    Update restaurantsList.jsx

--- restaurantsList.jsx
+++ restaurantsList.jsx
@@ -1,18 +1,1 @@
-import { Restaurant } from "../Restaurant/restaurant";
-import { useState } from "react";
 
-export const RestaurantsList = ({ restaurants = [] }) => {
-  const [activeRest, setActiveRest] = useState(restaurants[0].id);
-  return (
-    <>
-      <ul style={{ display: "flex", gap: "5px" }}>
-        {restaurants.map((rest, index) => (
-          <li key={rest.id} style={{ listStyle: "none" }}>
-            <button onClick={() => setActiveRest(rest.id)}>{rest.name}</button>
-          </li>
-        ))}
-      </ul>
-      <Restaurant rest={restaurants.find((item) => activeRest === item.id)} />
-    </>
-  );
-};
